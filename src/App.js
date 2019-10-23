0e4bd94a2d4054880657be9c5d4b06a63db1aa70
cmkishores <cmkishores@gmail.com>
2019-10-23 15:04:05 +0530

    Add Styles to app.css

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="FirstDiv">
     <h1>Hi, This is your To-do List</h1>
-    <div><input type="checkbox"/>
+    <div className="Checkbox-div"><input type="checkbox"/>
     <p>Complete the to-do Application</p>
     </div>
-    <div><input type="checkbox"/>
+    <div className="Checkbox-div"><input type="checkbox"/>
     <p>Complete the to-do Application</p>
     </div>
-    <div><input type="checkbox"/>
+    <div className="Checkbox-div"><input type="checkbox"/>
     <p>Complete the to-do Application</p>
     </div>
-    <div><input type="checkbox"/>
+    <div className="Checkbox-div"><input type="checkbox"/>
     <p>Complete the to-do Application</p>
     </div>
     </div>

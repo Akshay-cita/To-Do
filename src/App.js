a49011ff706c8a03779ebb9856044f32721a66b8
cmkishores <cmkishores@gmail.com>
2019-10-23 16:01:09 +0530

    Add Todo component to be reused to reduce code, Removed the if else parts from app.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   const date = new Date();
   const hour = date.getHours();
-  let welcometext;
-  const styles = {
-    fontSize: 30
-  };
-  if (hour > 6 && hour < 12) {
-    styles.color = "orange"
-    welcometext = "This is morning!"
-  }
-  else if (hour > 12 && hour < 16) {
-    styles.color = "red";
-    welcometext = "This is AfterNoon!";
-  }
-  else if (hour > 16 && hour < 20) {
-    styles.color = "brown";
-    welcometext = "This is evening!";
-  }
-  else if (hour > 20 && hour < 25 || hour < 6 ) {
-    styles.color = "black";
-    welcometext = "This is night!";
-  }
   
   return (
     <div className="FirstDiv">
     <h1>Hi, It is {date.getHours() %12}'O clock already! </h1>
-    <div style={{styles}}><input type="checkbox"/>
-    <p>{welcometext}</p>
-    </div>
-    <div style={{styles}}><input type="checkbox"/>
-    <p>{welcometext}</p>
-    </div>
-    <div style={{styles}}><input type="checkbox"/>
-    <p>{welcometext}</p>
-    </div>
-    <div style={{styles}}><input type="checkbox"/>
-    <p>{welcometext}</p>
-    </div>
+      <Todo />
+      <Todo />
+      <Todo /> 
     </div>
     
   );
---
+++ src/Todo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Todo() {
+    return (<div ><input type="checkbox"/>
+    <p>Did you do this?</p>
+    </div>)
+}
+export default Todo;

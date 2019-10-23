fd3bb60dc8af877e9db10c848d456c7238623f63
cmkishores <cmkishores@gmail.com>
2019-10-23 15:19:45 +0530

    Add const variables and display using literals

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const date = new Date();
+  const string1 = "This is the first task!"
+  const string4 = "This is the last task!"
+  const string3 = "This is the third task!"
+  const string2 = "This is the second task!"
   return (
     <div className="FirstDiv">
-    <h1>Hi, This is your To-do List</h1>
+    <h1>Hi, It is {date.getHours() %12}'O clock already! </h1>
     <div className="Checkbox-div"><input type="checkbox"/>
-    <p>Complete the to-do Application</p>
+    <p>{string1}</p>
     </div>
     <div className="Checkbox-div"><input type="checkbox"/>
-    <p>Complete the to-do Application</p>
+    <p>{string2}</p>
     </div>
     <div className="Checkbox-div"><input type="checkbox"/>
-    <p>Complete the to-do Application</p>
+    <p>{string3}</p>
     </div>
     <div className="Checkbox-div"><input type="checkbox"/>
-    <p>Complete the to-do Application</p>
+    <p>{string4}</p>
     </div>
     </div>
     

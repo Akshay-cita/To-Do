0027f6b8be49c0ddef6c633c530149975ba51e12
cmkishores <cmkishores@gmail.com>
2019-10-24 21:51:13 +0530

    Add extra key value pair to object in todolist

--- src/todolist.js
+++ src/todolist.js
@@ -1,28 +1,34 @@
 const TodoList = [
     {
         id:1,
-        todo: "Do something special"
+        todo: "Do something special",
+        completed: true
     },
 
     {
         id:2,
-        todo: "Do something adventurous"
+        todo: "Do something adventurous",
+        completed: true
     },
     {
         id:3,
-        todo: "Do something dangerous"
+        todo: "Do something dangerous",
+        completed: true
     },
     {
         id:4,
-        todo: "Do something daily"
+        todo: "Do something daily",
+        completed: true
     },
     {
         id:5,
-        todo: "Do something interesting"
+        todo: "Do something interesting",
+        completed: true
     },
     {
         id:6,
-        todo: "Do something different"
+        todo: "Do something different",
+        completed: true
     }
 ]
 export default TodoList;

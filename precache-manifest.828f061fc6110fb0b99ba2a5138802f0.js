15cab636d510896d8e4e8972c1b013e331b9e40f
cmkishores <cmkishores@gmail.com>
2019-10-25 16:21:07 +0530

    Updates

---
+++ precache-manifest.828f061fc6110fb0b99ba2a5138802f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8f450d349b128f2ae6829c238eeee2",
+    "url": "/To-Do/index.html"
+  },
+  {
+    "revision": "6a05965b37827cb2caf1",
+    "url": "/To-Do/static/css/main.c4134c41.chunk.css"
+  },
+  {
+    "revision": "998c12d260399d0b9b91",
+    "url": "/To-Do/static/js/2.8273d67e.chunk.js"
+  },
+  {
+    "revision": "6a05965b37827cb2caf1",
+    "url": "/To-Do/static/js/main.e6b3cfe2.chunk.js"
+  },
+  {
+    "revision": "2f4d5556fe0cc4762dca",
+    "url": "/To-Do/static/js/runtime-main.3261b5c1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/To-Do/static/media/logo.25bf045c.svg"
+  }
+]);
